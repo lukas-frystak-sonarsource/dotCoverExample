0dd8fc4bda2d4bafdb0d677ec38ffcf6ef7735ca
lukas-frystak-sonarsource <lukas.frystak@sonarsource.com>
2022-07-21 12:24:17 +0200

    Initial commit, project setup

---
+++ src/MathLibrary/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
