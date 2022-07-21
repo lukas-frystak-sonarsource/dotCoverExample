8bdd921d30fb0118ec0e526fa84af188b8f16461
lukas-frystak-sonarsource <lukas.frystak@sonarsource.com>
2022-07-21 14:03:02 +0200

    Add test project

---
+++ src/MathLibraryTests/CalculatorTests.cs
@@ -0,0 +1,51 @@
+using NUnit.Framework;
+
+namespace CalculatorTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Nothing here yet.
+        }
+
+        [Test]
+        public void CalculatorAdd()
+        {
+            // Expected
+            int expected = 3;
+
+            // Actual
+            int actual = Calculator.Add(1, 2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(5, 2, 3)]
+        [TestCase(10, 2, 8)]
+        public void CalculatorSubtract(int a, int b, int expected)
+        {
+            // Expected
+
+            // Actual
+            int actual = Calculator.Subtract(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(10, 2, 5)]
+        public void CalculatorDivide(double a, double b, double expected)
+        {
+            // Expected
+
+            // Actual
+            double actual = Calculator.Divide(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
