40567f73d8a2906bebf544a6cd503d6622624fe6
lukas-frystak-sonarsource <lukas.frystak@sonarsource.com>
2022-07-21 15:22:22 +0200

    run script done

--- run.ps1
+++ run.ps1
@@ -1,31 +1,27 @@
 # Constants
+$sonarQubeUrl = $env:SONARQUBE_URL
+$sonarQubeToken = $env:SONARQUBE_TOKEN
 $solution = ".\src\main.sln"
-$sonarQubeUrl = "http://localhost:9000"
-$sonarQubeToken = ""
 $projectKey = "example:dotCover"
 $projectName = "Example: dotCover .NET Coverage"
 $version = "1.0.0"
-$branch = "dotCover-Setup"
 
-$coverageReportOutputPath = ".\TestResults\dotCover.Output.html"
+$coverageReportPath = ".\TestResults\dotCover.Output.html"
+$testReportPath = ".\TestResults\testResults.trx"
 
 # Prepare analysis
 dotnet sonarscanner begin `
     /key:$projectKey /name:$projectName /v:$version `
     /d:sonar.host.url=$sonarQubeUrl /d:sonar.login=$sonarQubeToken `
-    /d:sonar.branch.name=$branch `
+    /d:sonar.cs.dotcover.reportsPaths=$coverageReportPath `
+    /d:sonar.cs.vstest.reportsPaths=$testReportPath `
     /d:sonar.verbose=false
 
-if (-not $?) {
-    Write-Warning "Prepare analysis step failed!"
-    Exit 1
-}
-
 # Build the solution
-dotnet build $solution
+dotnet build $solution --configuration Release
 
 # Run tests
-#dotnet dotcover test $solution --no-build --dcReportType=html --dcOutput=$coverageReportOutputPath
+dotnet dotcover test $solution --no-build --configuration Release --dcReportType=html --dcOutput=$coverageReportPath --logger "trx;logfilename=$testReportPath"
 
 # Run code analysis
 dotnet sonarscanner end /d:sonar.login=$sonarQubeToken
