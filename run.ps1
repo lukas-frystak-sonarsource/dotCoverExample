f9f4e4c63fa1d2418ff9a942cac8f1554f3bcb5a
lukas-frystak-sonarsource <lukas.frystak@sonarsource.com>
2022-07-21 16:44:34 +0200

    Add cleaning of existing test results

--- run.ps1
+++ run.ps1
@@ -6,8 +6,25 @@ $projectKey = "example:dotCover"
 $projectName = "Example: dotCover .NET Coverage"
 $version = "1.0.0"
 
-$coverageReportPath = ".\TestResults\dotCover.Output.html"
-$testReportPath = ".\TestResults\testResults.trx"
+$coverageReportDirectory = ".\TestResults"
+$coverageReportPath = "$coverageReportDirectory\dotCover.Output.html"
+# The unit test results are store in each test project: *Tests\TestResults\*.trx
+$testReportPath = ".\**\*.trx"
+
+# Firstly, clean existing test results if any exist. We need to do this as 
+# the *.trx report files are not overriden with subsequent test runs.
+# Clean dotCover test results
+if (Test-Path $coverageReportDirectory) {
+    Write-Host "Removing dotCover test results directory (""$coverageReportDirectory"")"
+    Remove-Item -Path $coverageReportDirectory -Recurse -Force
+} 
+
+# Clear unit test execution results from the respective test projects
+$testResultsDirectories = Get-ChildItem -Path .\**\*Tests\TestResults -Directory
+if ($null -ne $testResultsDirectories) {
+    Write-Host "Removing test result directories..."
+    $testResultsDirectories.FullName | Remove-Item -Recurse -Force
+}
 
 # Prepare analysis
 dotnet sonarscanner begin `
@@ -21,7 +38,7 @@ dotnet sonarscanner begin `
 dotnet build $solution --configuration Release
 
 # Run tests
-dotnet dotcover test $solution --no-build --configuration Release --dcReportType=html --dcOutput=$coverageReportPath --logger "trx;logfilename=$testReportPath"
+dotnet dotcover test $solution --no-build --configuration Release --dcReportType=html --dcOutput=$coverageReportPath --logger trx
 
 # Run code analysis
 dotnet sonarscanner end /d:sonar.login=$sonarQubeToken
