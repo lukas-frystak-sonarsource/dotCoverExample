584719b5eac95745fb204758dc69c03e7bc99403
lukas-frystak-sonarsource <lukas.frystak@sonarsource.com>
2022-07-21 16:50:12 +0200

    Edit comments

--- run.ps1
+++ run.ps1
@@ -14,21 +14,22 @@ $testReportPath = ".\**\*.trx"
 # =====================================================================================================================================
 
 # Firstly, clean existing test results if any exist. We need to do this as 
-# the *.trx report files are not overriden with subsequent test runs.
-# Clean dotCover test results
+# the *.trx report files are not overriden with subsequent test runs, so
+# the results would be counted multiple times.
+# Clean dotCover test results.
 if (Test-Path $coverageReportDirectory) {
     Write-Host "Removing dotCover test results directory (""$coverageReportDirectory"")"
     Remove-Item -Path $coverageReportDirectory -Recurse -Force
 } 
 
-# Clear unit test execution results from the respective test projects
+# Clear unit test execution results from the respective test projects.
 $testResultsDirectories = Get-ChildItem -Path .\**\*Tests\TestResults -Directory
 if ($null -ne $testResultsDirectories) {
     Write-Host "Removing test results directories..."
     $testResultsDirectories.FullName | Remove-Item -Recurse -Force
 }
 
-# Prepare analysis
+# Prepare SonarQube analysis.
 dotnet sonarscanner begin `
     /key:$projectKey /name:$projectName /v:$version `
     /d:sonar.host.url=$sonarQubeUrl /d:sonar.login=$sonarQubeToken `
@@ -36,11 +37,11 @@ dotnet sonarscanner begin `
     /d:sonar.cs.vstest.reportsPaths=$testReportPath `
     /d:sonar.verbose=false
 
-# Build the solution
+# Build the solution.
 dotnet build $solution --configuration Release
 
-# Run tests
+# Run tests.
 dotnet dotcover test $solution --no-build --configuration Release --dcReportType=html --dcOutput=$coverageReportPath --logger trx
 
-# Run code analysis
+# Run SonarQube code analysis.
 dotnet sonarscanner end /d:sonar.login=$sonarQubeToken
