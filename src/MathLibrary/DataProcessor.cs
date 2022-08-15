550bba81a8cdef3c39d29b622998bf011462b42c
lukas-frystak-sonarsource <lukas.frystak@sonarsource.com>
2022-08-15 17:01:12 +0200

    new prop

---
+++ src/MathLibrary/DataProcessor.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace MathLibrary
+{
+    public class DataProcessor
+    {
+        private readonly string _instance = string.Empty;
+
+        public DataProcessor()
+        {
+
+        }
+
+        public string Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+}
